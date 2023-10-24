be3f1b1ea28a01599c85af426a4bd7fbb605a1f3
Randy Truong <randyrtt12@gmail.com>
2023-10-24 15:44:15 -0500

    wrapped script in function for export

--- category_dict.py
+++ category_dict.py
@@ -2,26 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 from collections import defaultdict
 
-sreddit_in_cat = defaultdict(list)
 
-for i in range(1, 5):
-    url = f"https://www.reddit.com/best/communities/{i}/"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        parent_div = soup.find("div", class_="community-list")
+def getList():
+    sreddit_in_cat = defaultdict(list)
+    for i in range(1, 5):
+        url = f"https://www.reddit.com/best/communities/{i}/"
+        response = requests.get(url)
 
-        if parent_div:
-            subreddit = parent_div.find_all("div", recursive=False)
-            for div in subreddit:
-                # print(div.get("id"))
-                text_div = div.find_all("div")[0]
-                sreddit_name = text_div.find_all("a")[0].text.strip()
-                category = text_div.find_all("h6")[0].text.strip()
-                sreddit_in_cat[category].append(sreddit_name[2:])
-    else:
-        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, "html.parser")
+            parent_div = soup.find("div", class_="community-list")
 
-for k, v in sreddit_in_cat.items():
-    print(f"{k} {v}")
+            if parent_div:
+                subreddit = parent_div.find_all("div", recursive=False)
+                for div in subreddit:
+                    # print(div.get("id"))
+                    text_div = div.find_all("div")[0]
+                    sreddit_name = text_div.find_all("a")[0].text.strip()
+                    category = text_div.find_all("h6")[0].text.strip()
+                    sreddit_in_cat[category].append(sreddit_name[2:])
+        else:
+            print(
+                f"Failed to retrieve the webpage. Status code: {response.status_code}"
+            )
+    return sreddit_in_cat
+
+
+# sreddit_in_cat = getList()
+# for k, v in sreddit_in_cat.items():
+#     print(f"{k} {v}")
