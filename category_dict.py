2359cce0fedc5a570428baddb04e9bbd5a5ebe0a
Sirasornt <sirasornt@gmail.com>
2023-10-25 10:21:54 -0500

    add subs param to getList

--- category_dict.py
+++ category_dict.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 from collections import defaultdict
 
 
-def getList():
+def getList(subs_wanted):
     sreddit_in_cat = defaultdict(list)
+    subs_count = 0
     for i in range(1, 5):
         url = f"https://www.reddit.com/best/communities/{i}/"
         response = requests.get(url)
-
+        
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, "html.parser")
             parent_div = soup.find("div", class_="community-list")
@@ -16,18 +17,17 @@ def getList():
             if parent_div:
                 subreddit = parent_div.find_all("div", recursive=False)
                 for div in subreddit:
-                    # print(div.get("id"))
+                    if subs_count == subs_wanted:
+                        return sreddit_in_cat
                     text_div = div.find_all("div")[0]
                     sreddit_name = text_div.find_all("a")[0].text.strip()
                     category = text_div.find_all("h6")[0].text.strip()
                     sreddit_in_cat[category].append(sreddit_name[2:])
+                    subs_count +=1
         else:
-            print(
-                f"Failed to retrieve the webpage. Status code: {response.status_code}"
-            )
+            print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+
     return sreddit_in_cat
 
 
-# sreddit_in_cat = getList()
-# for k, v in sreddit_in_cat.items():
-#     print(f"{k} {v}")
+
