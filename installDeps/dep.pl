a60d56630a99efe80d9d00c78de89688632f3d9f
Randy Truong <randyrtt12@gmail.com>
2023-10-28 20:29:44 -0500

    fixed os compat

--- installDeps/dep.pl
+++ installDeps/dep.pl
@@ -5,11 +5,11 @@ use warnings;
 
 sub installDep() {
   print "Installing wheels... \n";
-  system "pip install wheel --break-system-packages";
+  system "pip install wheel";
 
   print "Installing mkl_fttl 1.3.3... \n";
   system "wget https://github.com/IntelPython/mkl_fft/archive/refs/tags/v1.3.3.zip";
-  system "pip install ./v1.3.3.zip --break-system-packages";
+  system "pip install ./v1.3.3.zip";
   print "Cleaning up mkl_fttl 1.3.3... \n";
   system "rm -rf ./v1.3.3.zip";
   print "Finished installing mkl_fttl 1.3.3... \n";
@@ -18,7 +18,7 @@ sub installDep() {
   system "clear";
   print "Installing mkl_random v1.2.3... \n";
   system "wget https://github.com/IntelPython/mkl_random/archive/refs/tags/v1.2.3.zip";
-  system "pip install ./v1.2.3.zip --break-system-packages";
+  system "pip install ./v1.2.3.zip";
   print "Cleaning up mkl_random v1.2.3... \n";
   system "rm -rf ./v1.2.3.zip";
   print "Finished installing mkl_random v1.2.3... \n";
@@ -26,7 +26,7 @@ sub installDep() {
   sleep(2);
   print "Installing mkl_service v2.4.0... \n";
   system "wget https://github.com/IntelPython/mkl-service/archive/refs/tags/v2.4.0.zip";
-  system "pip install ./v2.4.0.zip --break-system-packages";
+  system "pip install ./v2.4.0.zip";
   print "Cleaning up mkl_random v2.4.0... \n";
   system "rm -rf ./v2.4.0.zip";
   print "Finished installing mkl_random v2.4.0... \n";
@@ -34,14 +34,14 @@ sub installDep() {
   sleep(2);
   print "Installing intel-openmp 2021.4.0 \n";
   system "wget https://files.pythonhosted.org/packages/08/a0/eacf78b5d87cec6c6664d018b48d65051bf70bc43098e6fa589b17ae9f48/intel_openmp-2021.4.0-py2.py3-none-manylinux1_x86_64.whl";
-  system "pip install intel_openmp-2021.4.0-py2.py3-none-manylinux1_x86_64.whl --break-system-packages";
+  system "pip install intel_openmp-2021.4.0-py2.py3-none-manylinux1_x86_64.whl";
   print "Cleaning up intel-openmp 2021.4.0 \n";
-  system "rm -rf intel_openmp-2021.4.0-py2.py3-none-manylinux1_x86_64.whl --break-system-packages";
+  system "rm -rf intel_openmp-2021.4.0-py2.py3-none-manylinux1_x86_64.whl";
   print "Finished installing intel-openmp 2021.4.0 \n";
 
   sleep(2);
   print "Installing mkl 2021.4.0 \n";
-  system "pip install mkl==2021.4.0 --break-system-packages";
+  system "pip install mkl==2021.4.0";
   print "Finished installing mkl 2021.4.0 \n";
 
 
